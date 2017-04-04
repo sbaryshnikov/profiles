cded5957c5005bb7c8acd85e417ebf5a71d059f4
sbaryshnikov <sega@bk.ru>
2017-04-04 17:23:00 +0300

    update with latest package

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -4,8 +4,8 @@ class profiles::nginx (
   include ::nginx
   $vhosts.each |$vhost| {
     ::nginx::resource::server { $vhost['fqdn']:
-      port => $vhost['port'],
-      server_aliases => $vhost['server_aliases'],
+      listen_port => $vhost['port'],
+#      server_aliases => $vhost['server_aliases'],
       www_root => $vhost['www_root'],
     }
   }
