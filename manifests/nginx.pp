0b77bc6370fc77fbd01e65e3f1bff155fb12637d
sbaryshnikov <sega@bk.ru>
2017-04-04 16:59:59 +0300

    Add missing bracket

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -4,8 +4,9 @@ class profiles::nginx (
   include ::nginx
   $vhosts.each |$vhost| {
     ::nginx::resource::server { $vhost['fqdn']:
-    port => $vhost['port'],
-    server_aliases => $vhost['server_aliases'],
-    www_root => $vhost['www_root'],
+      port => $vhost['port'],
+      server_aliases => $vhost['server_aliases'],
+      www_root => $vhost['www_root'],
+    }
   }
 }
